37fcdadea44a926b2560b057b7da2e488d8a91a3
Aryan Malewar <108799982+beatsea20@users.noreply.github.com>
2024-01-10 20:00:15 +0530

    Update stablediffusionxl.py

--- StableDiffusionXL-panorama.py
+++ StableDiffusionXL-panorama.py
@@ -34,36 +34,48 @@ prompt1= "coherent 360-degree panorama image, The generated panorama’s ends sh
 image1 = pipe(
     prompt1,
     cross_attention_kwargs={"scale": 0.8},
+    num_inference_steps = 50,
     height = 512,
     width = 1024,
 ).images[0]
 image1.save("/content/drive/MyDrive/Avataar.ai/image1.png")
 
-prompt2= "coherent 360-degree panorama image, The generated panorama’s ends should meet i.e. seam needs to be tiled. A small salon of just 1 room, with mirrors."
+prompt2= "coherent 360-degree panorama image, The generated panorama’s ends should meet i.e. seam needs to be tiled. A small 4-walled salon with multiple mirrors, salon chairs, white walls, warm lighting, daylight."
 image2 = pipe(
     prompt2,
     cross_attention_kwargs={"scale": 0.8},
+    num_inference_steps = 50,
     height = 512,
     width = 1024,
 ).images[0]
 image2.save("/content/drive/MyDrive/Avataar.ai/image2.png")
 
-prompt3 = "a high resolution, hd, coherent 360 degree panorama image with seam tiling i.e. panorama's ends must meet, an empty court room resembling those in bollywood movies"
+prompt3 = "a high resolution, hd, coherent 360 degree panorama image with seam tiling i.e. panorama's ends must meet, an empty court room resembling those in movies, typical lighting, old architechture"
 image3 = pipe(
     prompt3,
     cross_attention_kwargs={"scale": 0.9},
+    num_inference_steps = 50,
     height = 512,
     width = 1024,
 ).images[0]
 image3.save("/content/drive/MyDrive/Avataar.ai/image3.png")
 
-prompt4= " a high resolution, coherent, 360-degree panoramic view of a gaming room with monitors displaying modern warfare games and a gamer sitting in front of one of the seats.The room is lit up by rgb lights. ensure seam tiling"
+prompt4= " a high resolution, coherent, 360-degree panoramic view of a gaming room with monitors displaying modern warfare games and a gamer sitting in front of one of the seats.The room is lit up by warm lights. ensure seam tiling"
 image4 = pipe(
     prompt4,
     cross_attention_kwargs={"scale": 0.0},
+    num_inference_steps = 50,
     height = 512,
     width = 1024,
-)
-image4_array = image4.images[0]
-image4_array.save("/content/drive/MyDrive/Avataar.ai/image4.png")
+).images[0]
+image4.save("/content/drive/MyDrive/Avataar.ai/image4.png")
 
+prompt5= "ancient room for keeping weapons, 360-degree panoramic image, detailed, 4k, old architechture, well preserved weapons, spectacular details"
+image5 = pipe(
+    prompt5,
+    cross_attention_kwargs={"scale": 0.8},
+    num_inference_steps = 50,
+    height = 512,
+    width = 1024,
+).images[0]
+image5.save("/content/drive/MyDrive/Avataar.ai/image5.png")
